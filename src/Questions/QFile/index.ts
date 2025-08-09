ac64dbfdb438eb6b9880570845baeb3254a21cdf
denmigda <denis.migdal@uca.fr>
2025-08-09 06:43:15 +0200

    Add default option to QFile

--- src/Questions/QFile/index.ts
+++ src/Questions/QFile/index.ts
@@ -7,23 +7,30 @@ const css  = require('!!raw-loader!./index.css' ).default;
 import {css as answer_css, setAnswerColor, setComment, setGlobalGrade} from "../QText/";
 import { upload } from "@TPEngine/utils/upload";
 
-class QFile extends LISS({html, css:[answer_css, css]}, WithBare, WithContent, WithRWValue<{content: string, type:string}>, WithMeta<AnswerMeta>) {
+export type File = {content: string, type:string};
+
+export class QFile extends LISS({html, css:[answer_css, css]},
+                                WithBare,
+                                WithContent,
+                                WithRWValue<File>,
+                                WithMeta<AnswerMeta>) {
 
     readonly pts        = +this.host.getAttribute("pts")!;
-    readonly hasViewer  = this.host.getAttribute("viewer")! !== "false";
+    readonly hasViewer  =  this.host.getAttribute("viewer")! !== "false";
+    readonly accepts    =  this.host.getAttribute('type') ?? "";
+    readonly default    =  this.host.getAttribute("default")!;
 
     readonly span_grade = this.content.querySelector<HTMLElement>('.grade')!;
     readonly answer     = this.content.querySelector<HTMLIFrameElement>('.answer')!;
 
-    readonly accepts    = this.host.getAttribute('type') ?? "";
-
     constructor() {
         super();
         
+        // selected
         this.content.querySelector<HTMLElement>('.upload_btn')!.addEventListener('click', async () => {
 
             const file = (await upload(this.accepts))!;
-            const data        = URL.createObjectURL(file);
+            const data = URL.createObjectURL(file);
 
             if( this.hasViewer )
                 this.answer.src = data;
@@ -61,14 +68,32 @@ class QFile extends LISS({html, css:[answer_css, css]}, WithBare, WithContent, W
         })
 
 
-        this._input.listen( () => {
+        this._input.listen( async () => {
 
             const value = this._input.value;
-            if( value === null)
-                return;
-            const {type, content} = value;
-            // @ts-ignore : fromBase64 should be added in Edge/Chrome soon.
-            const file = new Blob([Uint8Array.fromBase64(content)], {type});
+
+            let file   : Blob;
+            let type   : string;
+            let content: string;
+
+            if( value === null) {
+                if( this.default === null)
+                    return;
+
+                const rep  = await fetch(this.default);
+                file = await rep.blob();
+
+                type    = file.type;
+                // @ts-ignore
+                content = (await file.bytes()).toBase64();
+
+            } else {
+                ({type, content} = value);
+
+                // @ts-ignore : fromBase64 should be added in Edge/Chrome soon.
+                file = new Blob([Uint8Array.fromBase64(content)], {type});
+            }
+
             const data = URL.createObjectURL(file);
 
             if( this.hasViewer )
