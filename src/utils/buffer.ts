54c82e1e8e80fe7d5bc9eb209a608b30caaa26fe
denmigda <denis.migdal@uca.fr>
2025-09-12 13:00:42 +0200

    Fix issue when buffer2str array is too big

--- src/utils/buffer.ts
+++ src/utils/buffer.ts
@@ -1,7 +1,22 @@
 // can't use Uint16Array (a shame...)
 export function buffer2str(array: ArrayBuffer) {
 
-    return String.fromCharCode(... new Uint8Array(array) );
+    const bytes = new Uint8Array(array);
+
+    let str: string = "";
+    let offset = 0;
+    
+    // we need to use chunks if array is too big.
+    while(offset < bytes.length) {
+
+        const len   = Math.min(bytes.length - offset, 256*1024);
+        const chunk = bytes.slice(offset, offset + len);
+
+        str += String.fromCharCode(... chunk );
+        offset += len;
+    }
+
+    return str;
 }
 export function str2buffer(str: string) {
 
